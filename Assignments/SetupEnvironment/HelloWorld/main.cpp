e66ef3c9961144372a00b7bf3ed55d0a0b66d829
SebastianHall69 <sebastianhall13@gmail.com>
2019-02-24 21:39:13 -0800

    Setup environment assignment

---
+++ Assignments/SetupEnvironment/HelloWorld/main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: Sebastian Hall
+ * Created on February 14, 2019, 4:09 PM
+ * Purpose: Say hello to the world
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << "Hello World!";
+    //A Sebastian Production
+    return 0;
+}
